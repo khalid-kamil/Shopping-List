5bc76522ac058f0f2d5e326285fc2b8dc43023b0
Khalid <khalidkamil@icloud.com>
2020-11-25 00:37:18 +0000

    Shopping List - Add UITableView with swipe to delete - Add rightBarButtonItem to add new item to shopping list - Add leftBarButtonItem to clear shopping list

--- Milestone3/ViewController.swift
+++ Milestone3/ViewController.swift
@@ -7,13 +7,68 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    var shoppingList = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setUpNavBar()
+        
+        if shoppingList.isEmpty {
+            shoppingList = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"]
+        }
     }
-
+    
+    func setUpNavBar() -> Void {
+        title = "Shopping List"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptToAddItemToShoppingList))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Clear", style: .plain, target: self, action: #selector(clearShoppingList))
+    }
+    
+    @objc func promptToAddItemToShoppingList() -> Void {
+        let ac = UIAlertController(title: "New Item", message: "Add a new item to your shopping list", preferredStyle: .alert)
+        ac.addTextField()
+        let submitAction = UIAlertAction(title: "Submit", style: .default) {
+            [weak self, weak ac] _ in
+            guard let answer = ac?.textFields?[0].text else { return }
+            self?.submit(answer)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        ac.addAction(submitAction)
+        ac.addAction(cancelAction)
+        present(ac, animated: true)
+    }
+    
+    func submit(_ answer: String) {
+        if answer.count > 0 {
+            shoppingList.insert(answer, at: 0)
+            let indexPath = IndexPath(row: 0, section: 0)
+            tableView.insertRows(at: [indexPath], with: .automatic)
+        } else { return }
+    }
+    
+    @objc func clearShoppingList() {
+        shoppingList = []
+        tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shoppingList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Shopping Item", for: indexPath)
+        cell.textLabel?.text = shoppingList[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            shoppingList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
 
 }
 
